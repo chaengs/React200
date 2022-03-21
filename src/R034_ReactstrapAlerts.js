2fa7ccc1039a5fe464eb254972047a4588af5e4d
chaeng <cathyshim222@naver.com>
2022-03-21 15:17:49 +0900

    034 reactstrap Alerts 사용하기

---
+++ src/R034_ReactstrapAlerts.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Alert, UncontrolledAlert } from 'reactstrap';
+
+class R034_ReactstrapAlerts extends Component {
+    render() {
+        return (
+            <div>
+                <Alert color="light">
+                    Simple Alert [color : light]
+                </Alert>
+                <UncontrolledAlert color="warning">
+                    Uncontrolled Alert [color : warning]
+                </UncontrolledAlert>
+            </div>
+        )
+    }
+}
+
+export default R034_ReactstrapAlerts;
+
+//reactstrap color 속성 : primary, secondary, success, danger, warning, info, light, dark
