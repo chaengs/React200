39d5bb14d1bb4b7b8f70e8661d05a6a013a67c1b
chaeng <cathyshim222@naver.com>
2022-03-21 15:07:08 +0900

    032 Fragments 사용하기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import ReactHook from './R031_ReactHook'
+import Fragments from './R032_Fragments'
 
 function App() {
   return (
     <div>
       <h1>Start React 200</h1>
       <p>CSS 적용하기</p>
-      <ReactHook/>
+      <Fragments/>
     </div>
   );
 }
 
 export default App;
 
-// 함수형 컴포넌트에서는 state와 생명주기 함수와 같은 기능을 사용하기 위해 hook을 사용함.
-// 대표적인 hook 함수에는 usestate()와 useEffect()가 있음.
+// 컴포넌트 단위로 element를 return할 때 하나의 <html> 태그로 전체를 감싸지 않으면 에러가 발생.
+// <Fragments> 태그로 감싸면 불필요한 <html> 태그를 추가하지 않고 사용할 수 있음.
---
+++ src/R032_Fragments.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class R032_Fragments extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>P TAG</p>
+                <span>SPAN TAG</span>
+            </React.Fragment>
+        )
+    }
+}
+
+export default R032_Fragments;
+// <React.Fragment> 태그는 약식으로 <> </>로 사용가능.
