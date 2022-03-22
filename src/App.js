6c582c6062010ab2e7a458dac6bad0acc39c04de
chaeng <cathyshim222@naver.com>
2022-03-22 23:46:21 +0900

    036 reactstrap Breadcrumbs 사용하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ReactstrapBadges from './R035_ReactstrapBadges'
+import ReactstrapBreadcrumbs from './R036_ReactstrapBreadcrumbs'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,9 +8,11 @@ function App() {
     <div>
       <h1>Start React 200</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapBadges/>
+      <ReactstrapBreadcrumbs/>
     </div>
   );
 }
 
 export default App;
+
+//Breadcrumbs 패키지는 페지 위치 경로를 지정한 웹 내비게이션에 사용된다. 
