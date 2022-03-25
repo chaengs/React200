ab23cf7dc7c5c2e7ee5f81414ec6bec5f314b5fc
chaeng <cathyshim222@naver.com>
2022-03-25 16:58:10 +0900

    reactstrap Modal 사용하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ListGroup from './R047_ReactstrapListGroup';
+import Modal from './R048_ReactstrapModal';
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div>
-      <ListGroup/>
+      <Modal/>
     </div>
   );
 }
 
 export default App;
 
-// ListGroup 패키지는 정돈된 스타일의 목록을 표시할 때 사용.
+// Modal 패키지는 alert() 함수와 마찬가지로 사용자에게 원하는 시점에 알림창을 띄워 필요한 내용을 보여줌.
+// alert() 함수와 달리 웹 브라우저에서 팝업 창을 차단할 수 없고 배경 페이지와 어울리는 디자인을 적용할 수 있음.
