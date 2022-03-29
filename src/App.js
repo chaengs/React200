e4e4d81828a1300e57c5006fc974156bf5eb1b73
chaeng <cathyshim222@naver.com>
2022-03-29 22:40:50 +0900

    055 reactstrap Tab 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactstrapTable from './R054_ReactstrapTable';
+import ReactstrapTab from './R055_ReactstrapTab';
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div>
-      <ReactstrapTable />
+      <ReactstrapTab />
     </div>
   );
 }
 
 export default App;
+
+// Tab 패키지는 사용자 동작에 따라 특정 영역에 다른 내용을 표시할 때 사용
