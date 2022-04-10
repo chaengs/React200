cb8ad04884cfa48b50b9d5da807b758f8a6eab6a
chaeng <cathyshim222@naver.com>
2022-04-10 22:01:40 +0900

    077 컨텍스트로 부모 데이터 변경하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContextApi from './Context/R076_ContextApi'
+import ContextApi from './Context/R077_ContextApi'
 
 function App() {
   return (
@@ -10,7 +10,3 @@ function App() {
 }
 
 export default App;
-// 컨텍스트는 데이터의 공급자와 소비자를 정의하고 데이터가 필요한 컴포넌트만 사용할 수 있게 구현할 수 있음
-
-// 컨텍스트를 사용하면 하위 컴포넌트가 여러 개인 구조에서 유용하게 사용할 수 있음
-// 몇 번째 하위 컴포넌트인지와는 상관없이 필요한 하위 컴포넌트에서 소비자를 임포트해 필요한 데이터를 사용할 수 있다.
--- src/Context/R076_ContextApi.js
+++ src/Context/R076_ContextApi.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Children from './contextChildren';
+import Children from './contextChildren'; // contextChildren을 import
 
-const {Provider, Consumer} = React.createContext()
-export {Consumer}
+const {Provider, Consumer} = React.createContext() // react의 기본 함수인 createContext()를 호출, 공급자Provider와 소비자Consumer를 받음
+export {Consumer} // 하위 컴포넌트에서 소비자를 사용할 수 있도록 export
 
 class R076_ContextApi extends React.Component {
     render () {
-        return (
+        return ( // 자식 컴포넌트 Children을 <Provider>태그로 감싸고 전달할 데이터를 value값을
             <Provider value='React200'>
                 <Children />
             </Provider>
