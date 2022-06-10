5d6f595809fda6d2890aed4a877e065e40b91132
chaeng <cathyshim222@naver.com>
2022-06-10 20:59:50 +0900

    078 redux 리듀서로 스토어 생성하기

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'; //스토어 생성함수
+import reducers from './reducers';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+const store = createStore(reducers); //createStore 함수의 파라미터로 reducers 폴더 경로를 넘긴다.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const listener = () => {
+  ReactDOM.render(
+    <App store= {store}/>, //8번줄의 stroe를 App 컴포넌트에 전달. 
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(listener); // store 데이터에 변화가 있을 때 listener 함수 내부의 render 함수를 실행하고 변경된 데이터를 렌더링
+listener(); //수동으로 render 함수를 실행
